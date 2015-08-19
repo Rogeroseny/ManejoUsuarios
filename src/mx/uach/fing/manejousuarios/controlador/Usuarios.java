c8879e45e199a86ce2b66e931f73ab8f4dba8083
Rogelio Valenzuela <rogerval93@gmail.com>
2015-08-19 08:50:10 -0600

    primer_modificacion

---
+++ src/mx/uach/fing/manejousuarios/controlador/Usuarios.java
@@ -0,0 +1,19 @@
+package mx.uach.fing.manejousuarios.controlador;
+
+import java.util.ArrayList;
+import java.util.List;
+import mx.uach.fing.manejousuarios.datos.Usuario;
+
+/**
+ *  Clase que se encarga de manejar el proceso de altas. bajas y cambios y
+ * consultas de los usuarios.
+ * @author Rogelio Valenzuela
+ */
+public class Usuarios {
+    
+    public List<Usuario> getUsuarios(){
+        List<Usuario> usuarios = new ArrayList<>();
+        return usuarios;
+    }
+    
+}
